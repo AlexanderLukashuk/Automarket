583a733f58f1c907dd8147a53508e6a55e3a9687
AlexanderLukashuk <upiter465@gmail.com>
2023-07-04 20:11:26 +0600

    Added services for DbContext in Program class and added nuGet package

--- Automarket/Program.cs
+++ Automarket/Program.cs
@@ -1,4 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Automarket.DAL;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
