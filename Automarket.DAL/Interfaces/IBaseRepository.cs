b7409e4e88bdc4cb7281bbe929f947197096899a
AlexanderLukashuk <upiter465@gmail.com>
2023-07-06 12:16:00 +0600

    Created base generic interface for repositories

---
+++ Automarket.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Automarket.DAL.Interfaces
+{
+	public interface IBaseRepository<T>
+	{
+		bool Create(T entity);
+
+		T Get(int id);
+
+		IEnumerable<T> Select();
+
+		bool Delete(T entity);
+	}
+}
+
