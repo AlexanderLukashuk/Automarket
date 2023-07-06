86b1041a64be8b9aa49cf9f5acc5fff60e33e46c
AlexanderLukashuk <upiter465@gmail.com>
2023-07-06 12:45:42 +0600

    Created initial migration

--- Automarket/Program.cs
+++ Automarket/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(connectionString));
+        options.UseSqlServer(connectionString, optionsBuilder => optionsBuilder.MigrationsAssembly("Automarket")));
 
 builder.Services.AddScoped<ICarRepository, CarRepository>();
 
