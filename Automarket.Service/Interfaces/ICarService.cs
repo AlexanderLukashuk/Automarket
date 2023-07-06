33dd704cb669f224dbbb694f5e81690a55f73ae2
AlexanderLukashuk <upiter465@gmail.com>
2023-07-07 01:53:58 +0600

    Added methods in ICarService

--- Automarket.Service/Interfaces/ICarService.cs
+++ Automarket.Service/Interfaces/ICarService.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using Automarket.Domain.Entity;
 using Automarket.Domain.Response;
+using Automarket.Domain.ViewModels.Car;
 
 namespace Automarket.Service.Interfaces
 {
 	public interface ICarService
 	{
 		Task<IBaseResponse<IEnumerable<Car>>> GetCars();
+
+		Task<IBaseResponse<Car>> GetCar(int id);
+
+		Task<IBaseResponse<CarViewModel>> CreateCar(CarViewModel carViewModel);
+
+		Task<IBaseResponse<bool>> DeleteCar(int id);
+
+		Task<IBaseResponse<Car>> GetCarByName(string name);
 	}
 }
 
