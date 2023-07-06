029344d859a8f45a0c8ab530a34ce99e727a0664
AlexanderLukashuk <upiter465@gmail.com>
2023-07-06 19:49:42 +0600

    Created method GetCars() in ICarService

--- Automarket.Service/Interfaces/ICarService.cs
+++ Automarket.Service/Interfaces/ICarService.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using Automarket.Domain.Entity;
+using Automarket.Domain.Response;
+
 namespace Automarket.Service.Interfaces
 {
 	public interface ICarService
 	{
+		Task<BaseResponse<IEnumerable<Car>>> GetCars();
 	}
 }
 
