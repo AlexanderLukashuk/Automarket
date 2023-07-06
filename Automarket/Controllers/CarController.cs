69e2841eeb3796871dd558ee69638e01a336bd07
AlexanderLukashuk <upiter465@gmail.com>
2023-07-06 13:02:12 +0600

    Created CarController

---
+++ Automarket/Controllers/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Automarket.DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Automarket.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly ICarRepository _carRepository;
+
+        public CarController(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public async Task<IActionResult> GetCars()
+        {
+            var response = await _carRepository.Select();
+            return View(response);
+        }
+    }
+}
+
