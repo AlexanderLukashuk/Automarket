513ffbbdf208cc561ca7edb299a548c824ea0803
AlexanderLukashuk <upiter465@gmail.com>
2023-07-06 19:46:48 +0600

    Created generic interface IBaseResponse, added new property in BaseResponse class and made generic

--- Automarket.Domain/Response/BaseResponse.cs
+++ Automarket.Domain/Response/BaseResponse.cs
@@ -3,11 +3,18 @@ using Automarket.Domain.Enum;
 
 namespace Automarket.Domain.Response
 {
-	public class BaseResponse
+	public class BaseResponse<T> : IBaseResponse<T>
 	{
 		public string? Description { get; set; } // If an error occurs
 
 		public StatusCode StatusCode { get; set; } // Error code
+
+		public T? Data { get; }
     }
+
+	public interface IBaseResponse<T>
+	{
+		T? Data { get; }
+	}
 }
 
