04dfd1d88da677c526230adec608b8276786058c
AlexanderLukashuk <upiter465@gmail.com>
2023-07-07 19:07:10 +0600

    Implemented new methods of IBaseRepository interface in CarRepository class

--- Automarket.DAL/Repositories/CarRepository.cs
+++ Automarket.DAL/Repositories/CarRepository.cs
@@ -14,40 +14,21 @@ namespace Automarket.DAL.Repositories
             _context = context;
         }
 
-        public async Task<bool> Create(Car entity)
+        public async Task Create(Car entity)
         {
-            await _context.Cars.AddAsync(entity);
+            await _context.AddAsync(entity);
             await _context.SaveChangesAsync();
-
-            return true;
         }
 
-        public async Task<bool> Delete(Car entity)
+        public async Task Delete(Car entity)
         {
             _context.Remove(entity);
             await _context.SaveChangesAsync();
-
-            return true;
-        }
-
-        public async Task<Car> Get(int id)
-        {
-            return await _context.Cars.FirstOrDefaultAsync(c => c.Id == id);
         }
 
         public IQueryable<Car> GetAll()
         {
             return _context.Cars;
-        }
-
-        public async Task<Car> GetByName(string name)
-        {
-            return await _context.Cars.FirstOrDefaultAsync(c => string.Equals(c.Name, name));
-        }
-
-        public async Task<List<Car>> Select()
-        {
-            return await _context.Cars.ToListAsync();
         }
 
         public async Task<Car> Update(Car entity)
@@ -57,6 +38,50 @@ namespace Automarket.DAL.Repositories
 
             return entity;
         }
+
+        //public async Task<bool> Create(Car entity)
+        //{
+        //    await _context.Cars.AddAsync(entity);
+        //    await _context.SaveChangesAsync();
+
+        //    return true;
+        //}
+
+        //public async Task<bool> Delete(Car entity)
+        //{
+        //    _context.Remove(entity);
+        //    await _context.SaveChangesAsync();
+
+        //    return true;
+        //}
+
+        //public async Task<Car> Get(int id)
+        //{
+        //    return await _context.Cars.FirstOrDefaultAsync(c => c.Id == id);
+        //}
+
+        //public IQueryable<Car> GetAll()
+        //{
+        //    return _context.Cars;
+        //}
+
+        //public async Task<Car> GetByName(string name)
+        //{
+        //    return await _context.Cars.FirstOrDefaultAsync(c => string.Equals(c.Name, name));
+        //}
+
+        //public async Task<List<Car>> Select()
+        //{
+        //    return await _context.Cars.ToListAsync();
+        //}
+
+        //public async Task<Car> Update(Car entity)
+        //{
+        //    _context.Cars.Update(entity);
+        //    await _context.SaveChangesAsync();
+
+        //    return entity;
+        //}
     }
 }
 
