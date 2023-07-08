8aa890f93c8cbc419aecacb535044dd20433e82f
AlexanderLukashuk <upiter465@gmail.com>
2023-07-08 21:48:09 +0600

    Created UserRepository class

---
+++ Automarket.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Automarket.DAL.Interfaces;
+using Automarket.Domain.Entity;
+
+namespace Automarket.DAL.Repositories
+{
+	public class UserRepository : IBaseRepository<User>
+	{
+		private readonly ApplicationDbContext _context;
+
+		public UserRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+        public Task Create(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Update(User entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
