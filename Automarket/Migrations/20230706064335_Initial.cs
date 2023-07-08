dda09e549e451a3c8e0ff94d122767ee663e78cc
AlexanderLukashuk <upiter465@gmail.com>
2023-07-09 03:41:34 +0600

    Created new migrations

--- Automarket/Migrations/20230706064335_Initial.cs
+++ Automarket/Migrations/20230706064335_Initial.cs
@@ -11,24 +11,24 @@ namespace Automarket.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "Cars",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Desctiption = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Model = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Speed = table.Column<double>(type: "float", nullable: false),
-                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    DateCreate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    TypeCar = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Cars", x => x.Id);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "Cars",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(type: "int", nullable: false)
+            //            .Annotation("SqlServer:Identity", "1, 1"),
+            //        Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            //        Desctiption = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            //        Model = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            //        Speed = table.Column<double>(type: "float", nullable: false),
+            //        Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+            //        DateCreate = table.Column<DateTime>(type: "datetime2", nullable: false),
+            //        TypeCar = table.Column<int>(type: "int", nullable: false)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_Cars", x => x.Id);
+            //    });
         }
 
         /// <inheritdoc />
