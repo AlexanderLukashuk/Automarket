a7f2271badcdf4e325142d8f9a2210e531204d85
AlexanderLukashuk <upiter465@gmail.com>
2023-07-08 22:30:03 +0600

    Created EnumExtension in Domain folder

---
+++ Automarket.Domain/Extensions/EnumExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Automarket.Domain.Extensions
+{
+	public static class EnumExtension
+	{
+		public static string GetDisplayName(this System.Enum enumValue)
+		{
+			//return enumValue.GetType()
+			//	.GetMember(enumValue.ToString())
+			//	.First()
+			//	.GetCustomAttributes<DisplayAttribute>()
+   //             ?.GetName() ?? "Indefinite";
+
+			var memberInfo = enumValue.GetType()
+				.GetMember(enumValue.ToString())
+				.First();
+
+			if (memberInfo != null)
+			{
+				var displayAttribute = memberInfo.GetCustomAttribute<DisplayAttribute>();
+				if (displayAttribute != null)
+					return displayAttribute.Name;
+			}
+
+			return "Indefinite";
+        }
+	}
+}
+
