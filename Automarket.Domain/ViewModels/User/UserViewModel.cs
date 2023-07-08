8e817631a932f2aa152c70403839541eaee143a7
AlexanderLukashuk <upiter465@gmail.com>
2023-07-08 21:45:30 +0600

    Created UserViewModel

---
+++ Automarket.Domain/ViewModels/User/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Automarket.Domain.ViewModels.User
+{
+	public class UserViewModel
+	{
+		[Display(Name = "Id")]
+		public long Id { get; set; }
+
+		[Required(ErrorMessage = "Specify the role")]
+		[Display(Name = "Role")]
+		public string? Role { get; set; }
+
+		[Required(ErrorMessage = "Specify the name")]
+		[Display(Name = "Login")]
+		public string? Name { get; set; }
+
+		[Required(ErrorMessage = "Specify the password")]
+		[Display(Name = "Password")]
+		public string? Password { get; set; }
+	}
+}
+
