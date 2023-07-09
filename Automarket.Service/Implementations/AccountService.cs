01f4b8d697aca819478e03de156d2da86aec8a12
AlexanderLukashuk <upiter465@gmail.com>
2023-07-09 15:54:24 +0600

    Implemented IAccountService methods in AccountService

---
+++ Automarket.Service/Implementations/AccountService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Security.Claims;
+using Automarket.DAL.Interfaces;
+using Automarket.Domain.Entity;
+using Automarket.Domain.Enum;
+using Automarket.Domain.Helpers;
+using Automarket.Domain.Response;
+using Automarket.Domain.ViewModels.Account;
+using Automarket.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Automarket.Service.Implementations
+{
+	public class AccountService : IAccountService
+	{
+		private readonly IBaseRepository<User> _userRepository;
+
+		public AccountService(IBaseRepository<User> userRepository)
+		{
+			_userRepository = userRepository;
+		}
+
+        public async Task<BaseResponse<ClaimsIdentity>> Login(LoginViewModel model)
+        {
+            try
+            {
+                var user = await _userRepository.GetAll().FirstOrDefaultAsync(x => x.Name == model.Name);
+                if (user == null)
+                {
+                    return new BaseResponse<ClaimsIdentity>()
+                    {
+                        Description = "User not found"
+                    };
+                }
+
+                if (user.Password != HashPasswordHelper.HashPassword(model.Password))
+                {
+                    return new BaseResponse<ClaimsIdentity>()
+                    {
+                        Description = "Invalid password or login"
+                    };
+                }
+
+                var result = Authenticate(user);
+
+                return new BaseResponse<ClaimsIdentity>()
+                {
+                    Data = result,
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<ClaimsIdentity>()
+                {
+                    Description = ex.Message,
+                    StatusCode = StatusCode.InternalServerError
+                };
+            }
+        }
+
+        public async Task<BaseResponse<ClaimsIdentity>> Register(RegisterViewModel model)
+        {
+            try
+            {
+                var user = await _userRepository.GetAll().FirstOrDefaultAsync(x => x.Name == model.Name);
+                if (user != null)
+                {
+                    return new BaseResponse<ClaimsIdentity>()
+                    {
+                        Description = "A user with this username already exists"
+                    };
+                }
+
+                user = new User()
+                {
+                    Name = model.Name,
+                    Role = Role.User,
+                    Password = HashPasswordHelper.HashPassword(model.Password)
+                };
+
+                await _userRepository.Create(user);
+
+                var result = Authenticate(user);
+
+                return new BaseResponse<ClaimsIdentity>()
+                {
+                    Data = result,
+                    Description = "The object was added",
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<ClaimsIdentity>()
+                {
+                    Description = ex.Message,
+                    StatusCode = StatusCode.InternalServerError
+                };
+            }
+        }
+
+        private ClaimsIdentity Authenticate(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Name),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.ToString())
+            };
+
+            return new ClaimsIdentity(claims, "ApplicationCookie",
+                ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+        }
+    }
+}
+
