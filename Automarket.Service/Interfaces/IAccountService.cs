056a5f5b2ec326db18618f9434f9c98cc327d749
AlexanderLukashuk <upiter465@gmail.com>
2023-07-09 15:23:10 +0600

    Created IAccountService interface

---
+++ Automarket.Service/Interfaces/IAccountService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Claims;
+using Automarket.Domain.Response;
+using Automarket.Domain.ViewModels.Account;
+
+namespace Automarket.Service.Interfaces
+{
+	public interface IAccountService
+	{
+		Task<BaseResponse<ClaimsIdentity>> Register(RegisterViewModel model);
+
+		Task<BaseResponse<ClaimsIdentity>> Login(LoginViewModel model);
+	}
+}
+
