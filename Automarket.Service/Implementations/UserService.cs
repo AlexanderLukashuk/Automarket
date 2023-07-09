2574a35c66ca39b62da7cad899abfe3c00c2e278
AlexanderLukashuk <upiter465@gmail.com>
2023-07-09 15:08:02 +0600

    Implemented IUserService methods in UserService class

---
+++ Automarket.Service/Implementations/UserService.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Automarket.DAL.Interfaces;
+using Automarket.Domain.Entity;
+using Automarket.Domain.Enum;
+using Automarket.Domain.Extensions;
+using Automarket.Domain.Helpers;
+using Automarket.Domain.Response;
+using Automarket.Domain.ViewModels.User;
+using Automarket.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Automarket.Service.Implementations
+{
+	public class UserService : IUserService
+	{
+		//private readonly ILogger<UserService> _logger;
+		private readonly IBaseRepository<User> _userRepository;
+
+		public UserService(IBaseRepository<User> userRepository)
+		{
+			_userRepository = userRepository;
+		}
+
+        public async Task<IBaseResponse<User>> Create(UserViewModel model)
+        {
+            try
+            {
+                var user = await _userRepository.GetAll().FirstOrDefaultAsync(x => x.Name == model.Name);
+                if (user != null)
+                {
+                    return new BaseResponse<User>()
+                    {
+                        Description = "A user with this username already exists",
+                        StatusCode = StatusCode.UserAlreadyExists
+                    };
+                }
+
+                user = new User()
+                {
+                    Name = model.Name,
+                    Role = Enum.Parse<Role>(model.Role),
+                    Password = HashPasswordHelper.HashPassword(model.Password)
+                };
+
+                await _userRepository.Create(user);
+
+                return new BaseResponse<User>()
+                {
+                    Data = user,
+                    Description = "User added",
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<User>()
+                {
+                    StatusCode = StatusCode.InternalServerError,
+                    Description = $"Internal error: {ex.Message}"
+                };
+            }
+        }
+
+        public async Task<IBaseResponse<bool>> DeleteUser(long id)
+        {
+            try
+            {
+                var user = await _userRepository.GetAll().FirstOrDefaultAsync(x => x.Id == id);
+                if (user == null)
+                {
+                    return new BaseResponse<bool>()
+                    {
+                        StatusCode = StatusCode.UserNotFound,
+                        Data = false
+                    };
+                }
+
+                await _userRepository.Delete(user);
+
+                return new BaseResponse<bool>()
+                {
+                    StatusCode = StatusCode.OK,
+                    Data = true
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<bool>()
+                {
+                    StatusCode = StatusCode.InternalServerError,
+                    Description = $"Internal error: {ex.Message}"
+                };
+            }
+        }
+
+        public BaseResponse<Dictionary<int, string>> GetRoles()
+        {
+            try
+            {
+                var roles = ((Role[])Enum.GetValues(typeof(Role)))
+                    .ToDictionary(k => (int)k, t => t.GetDisplayName());
+
+                return new BaseResponse<Dictionary<int, string>>()
+                {
+                    Data = roles,
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<Dictionary<int, string>>()
+                {
+                    StatusCode = StatusCode.InternalServerError,
+                    Description = $"Internal error: {ex.Message}"
+                };
+            }
+        }
+
+        public async Task<BaseResponse<IEnumerable<UserViewModel>>> GetUsers()
+        {
+            try
+            {
+                var users = await _userRepository.GetAll()
+                    .Select(x => new UserViewModel()
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Role = x.Role.GetDisplayName()
+                    })
+                    .ToListAsync();
+
+                return new BaseResponse<IEnumerable<UserViewModel>>()
+                {
+                    Data = users,
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<IEnumerable<UserViewModel>>()
+                {
+                    StatusCode = StatusCode.InternalServerError,
+                    Description = $"Internal error: {ex.Message}"
+                };
+            }
+        }
+    }
+}
+
