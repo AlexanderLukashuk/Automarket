2cb0f14148cd051f9da5fd34614a23b61fe46536
AlexanderLukashuk <upiter465@gmail.com>
2023-07-11 20:36:41 +0600

    Created ProfileService class

---
+++ Automarket.Service/Implementations/ProfileService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Automarket.DAL.Interfaces;
+using Automarket.Domain.Entity;
+using Automarket.Domain.Enum;
+using Automarket.Domain.Response;
+using Automarket.Domain.ViewModels.Profile;
+using Automarket.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Automarket.Service.Implementations
+{
+	public class ProfileService : IProfileService
+	{
+		private readonly IBaseRepository<Profile> _profileRepository;
+
+		public ProfileService(IBaseRepository<Profile> profileRepository)
+		{
+			_profileRepository = profileRepository;
+		}
+
+        public async Task<BaseResponse<ProfileViewModel>> GetProfile(string userName)
+        {
+            try
+            {
+                var profile = await _profileRepository.GetAll()
+                    .Select(x => new ProfileViewModel()
+                    {
+                        Id = x.Id,
+                        Address = x.Address,
+                        Age = x.Age,
+                        UserName = x.Usser.Name
+                    })
+                    .FirstOrDefaultAsync(x => x.UserName == userName);
+
+                return new BaseResponse<ProfileViewModel>()
+                {
+                    Data = profile,
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<ProfileViewModel>()
+                {
+                    StatusCode = StatusCode.InternalServerError,
+                    Description = $"Internal error: {ex.Message}"
+                };
+            }
+        }
+
+        public async Task<BaseResponse<Profile>> Save(ProfileViewModel model)
+        {
+            try
+            {
+                var profile = await _profileRepository.GetAll()
+                    .FirstOrDefaultAsync(x => x.Id == model.Id);
+
+                profile.Address = model.Address;
+                profile.Age = model.Age;
+
+                await _profileRepository.Update(profile);
+
+                return new BaseResponse<Profile>()
+                {
+                    Data = profile,
+                    Description = "Data updated",
+                    StatusCode = StatusCode.OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse<Profile>()
+                {
+                    StatusCode = StatusCode.InternalServerError,
+                    Description = $"Internal error: {ex.Message}"
+                };
+            }
+        }
+    }
+}
+
