fef6c63b141cb6bb519bef8ae08df4348e9c0694
AlexanderLukashuk <upiter465@gmail.com>
2023-07-11 20:16:55 +0600

    Created IProfileService interface

---
+++ Automarket.Service/Interfaces/IProfileService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Automarket.Domain.Entity;
+using Automarket.Domain.Response;
+using Automarket.Domain.ViewModels.Profile;
+
+namespace Automarket.Service.Interfaces
+{
+	public interface IProfileService
+	{
+		Task<BaseResponse<ProfileViewModel>> GetProfile(string userName);
+
+		Task<BaseResponse<Profile>> Save(ProfileViewModel model);
+	}
+}
+
