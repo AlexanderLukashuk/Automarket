2154fce0e110a6c9751dc403543741254905f89a
AlexanderLukashuk <upiter465@gmail.com>
2023-07-11 19:44:39 +0600

    Created UserController

---
+++ Automarket/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Automarket.Domain.ViewModels.User;
+using Automarket.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Automarket.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IActionResult> GetUsers()
+        {
+            var response = await _userService.GetUsers();
+            if (response.StatusCode == Domain.Enum.StatusCode.OK)
+            {
+                return View(response.Data);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> DeleteUser(long id)
+        {
+            var response = await _userService.DeleteUser(id);
+            if (response.StatusCode == Domain.Enum.StatusCode.OK)
+            {
+                return RedirectToAction("GetUsers");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Save() => PartialView();
+
+        [HttpPost]
+        public async Task<IActionResult> Save(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _userService.Create(model);
+                if (response.StatusCode == Domain.Enum.StatusCode.OK)
+                {
+                    return Json(new { description = response.Description });
+                }
+                return BadRequest(new { errorMessage = response.Description });
+            }
+
+            var errorMessage = ModelState.Values
+                .SelectMany(v => v.Errors.Select(x => x.ErrorMessage))
+                .ToList()
+                .Join();
+
+            return StatusCode(StatusCodes.Status500InternalServerError, new { errorMessage });
+        }
+
+        [HttpPost]
+        public JsonResult GetRoles()
+        {
+            var types = _userService.GetRoles();
+            return Json(types.Data);
+        }
+    }
+}
+
