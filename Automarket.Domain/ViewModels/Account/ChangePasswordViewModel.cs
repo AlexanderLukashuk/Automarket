3082954a791c220cc676a6eb91ea60cc94aef88c
AlexanderLukashuk <upiter465@gmail.com>
2023-07-20 18:20:07 +0600

    Created ChangePasswordViewModel class

---
+++ Automarket.Domain/ViewModels/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Automarket.Domain.ViewModels.Account
+{
+	public class ChangePasswordViewModel
+	{
+		[Required(ErrorMessage = "Specify the Name")]
+		public string? UserName { get; set; }
+
+		[Required(ErrorMessage = "Enter the password")]
+		[DataType(DataType.Password)]
+		[Display(Name = "Password")]
+		[MinLength(5, ErrorMessage = "The password must be greater than or equal to 5 characters")]
+		public string? NewPassword { get; set; }
+	}
+}
+
