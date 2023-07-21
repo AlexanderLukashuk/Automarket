4671a602311aac12798162b23ba3fe0025c9d890
AlexanderLukashuk <upiter465@gmail.com>
2023-07-21 16:59:10 +0600

    Created BasketController

---
+++ Automarket/Controllers/BasketController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Automarket.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Automarket.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+
+        public async Task<IActionResult> Detail()
+        {
+            var response = await _basketService.GetItems(User.Identity.Name);
+            if (response.StatusCode == Domain.Enum.StatusCode.OK)
+            {
+                return View(response.Data.ToList());
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> GetItem(long id)
+        {
+            var response = await _basketService.GetItem(User.Identity.Name, id);
+            if (response.StatusCode == Domain.Enum.StatusCode.OK)
+            {
+                return PartialView(response.Data);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+
