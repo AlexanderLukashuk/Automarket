2f50fc685d2759938d72e577fcde167bd36b3622
AlexanderLukashuk <upiter465@gmail.com>
2023-07-21 16:32:25 +0600

    Created IBasketService interface

---
+++ Automarket.Service/Interfaces/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Automarket.Domain.Response;
+
+namespace Automarket.Service.Interfaces
+{
+	public interface IBasketService
+	{
+		Task<IBaseResponse<IEnumerable<OrderViewModel>>> GetItems(string userName);
+
+		Task<IBaseResponse<OrderViewModel>> GetItem(string userName, long id);
+	}
+}
+
