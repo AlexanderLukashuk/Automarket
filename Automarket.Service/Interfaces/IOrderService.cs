428660c34239c0bac04577dfb82fba4fa9894085
AlexanderLukashuk <upiter465@gmail.com>
2023-07-21 17:13:10 +0600

    Created IOrderService interface

---
+++ Automarket.Service/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Automarket.Domain.Entity;
+using Automarket.Domain.Response;
+
+namespace Automarket.Service.Interfaces
+{
+	public interface IOrderService
+	{
+		Task<IBaseResponse<Order>> Create(CreateOrderViewModel model);
+
+		Task<IBaseResponse<bool>> Delete(long id);
+	}
+}
+
